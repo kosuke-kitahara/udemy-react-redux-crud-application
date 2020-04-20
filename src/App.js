195169ebdda7283a6d126bdf3dc88d545c441eb0
kosuke-kitahara <kosuke.kitahara7@gmail.com>
2020-04-21 00:50:39 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    render() {
-        return <div><h1>Hello, world!</h1></div>;
-    }
+   render() {
+       return <div><h1>Hello, World!</h1></div>;
+   }
 }
+
 
 export default App;
